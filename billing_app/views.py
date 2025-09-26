b645092789282b16c3797c85bd591109c0b9cd1f
Joseph-Wairimu <josephkamore084@gmail.com>
2025-09-26 14:38:31 +0300

    update(core): billing allowed endpoints

--- billing_app/views.py
+++ billing_app/views.py
@@ -91,6 +91,8 @@ class BillingRecordViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsAdmin | IsSiteManagerForSite]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['customer', 'meter', 'reading_date', 'payment_status']
+    def get_http_method_names(self):
+        return ['get', 'post']
 
 
 class PaymentLogViewSet(viewsets.ReadOnlyModelViewSet):
