2f32c22f982b0708ef8e24aaee2a3f0d1b31badf
Joseph-Wairimu <josephkamore084@gmail.com>
2025-10-01 15:37:40 +0300

    update: allow user password setting

--- billing_app/serializers.py
+++ billing_app/serializers.py
@@ -7,9 +7,27 @@ from django.utils import timezone
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ['id', 'email', 'role']
+        fields = ['id', 'email', 'role', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)  
+        instance.save()
+        return instance
 
 
 class RoleSerializer(serializers.ModelSerializer):
